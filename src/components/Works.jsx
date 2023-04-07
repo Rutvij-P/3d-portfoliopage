425da85a157920e38d36900438c2ba8a45ad692a
Rutu Patel <patelrutvij2801@gmail.com>
2023-04-07 05:32:51 -0700

    Added tools 3D feature + working on works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+// import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>The Journey as of now ..</p>
+        <h2 className={styles.sectionHeadText}>Experience.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
